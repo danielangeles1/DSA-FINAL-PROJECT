11896f3a22f2e44b665f02f3a4251666f6bf6b43
danielangeles1 <111299637+danielangeles1@users.noreply.github.com>
2024-12-16 13:28:07 -0800

    Add files via upload

---
+++ PremiumBuyerInterface.java
@@ -0,0 +1,13 @@
+/**
+ * represents the interface for premium buyers
+ * provides a method to get the discount rate for premium  buyers
+ */
+public interface PremiumBuyerInterface {
+
+    /**
+     * Returns the discount rate for a premium buyer
+     * 
+     * @return the double discount rate
+     */
+    public double getDiscount();
+}
